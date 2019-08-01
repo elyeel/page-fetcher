b5a7db4802a6346ce5460a0e5f38ae6ffb30d7ab
elyeel <elyeel@yahoo.com>
2019-08-01 21:23:22 +0000

    file size worked out with write file

---
+++ fetcher.js
@@ -0,0 +1,36 @@
+const arg = process.argv;
+const address = arg[2];
+const file = arg[3];
+const request = require('request');
+const fs = require('fs');
+
+request(address, (error, response, body) => {
+  console.time();
+  fs.writeFile(file,body,(err)=> {
+    if (!err) {
+      console.timeEnd();
+      const stats = fs.statSync(file);
+      const fileSize = stats["size"];
+      console.log('Downloaded and saved ' + stats.size + ' bytes to ' + file);
+      
+      // setTimeout(() => {
+      //   const stats = fs.statSync(file);
+        
+      //   console.log("Found no error. Bytes = " + stats.size);
+
+      // }, 3000);
+      // console.log("no Error " + body.Buffer);
+      // const stream = fs.createReadStream('./test.html');
+      // console.log('Bytes = ' + stream.bytesRead);
+      // const stat = fs.stats;
+      // console.log("bytes = " + stream.bytesRead)
+      // console.log(stream);
+      // let result = fs.readFile(file,'utf8',(err,data) => {
+      //   console.log(data.statSync.bytesRead);
+      // })
+    };
+
+  })
+  
+})
+
